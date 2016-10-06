a1270551c97aa1dcf14c095b17a6a0af45e2734d
Fabien Foerster <fabienfoerster@gmail.com>
2016-10-06 20:58:24 +0200

    first draft of the client

--- src/App.js
+++ src/App.js
@@ -1,18 +1,48 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import * as firebase from 'firebase';
+
+import ReactPlayer from 'react-player';
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      url: "https://media.giphy.com/media/RQIhi5u8eLEsM/giphy.gif",
+      mimetype: "image/gif"
+    };
+  }
+
+  componentDidMount() {
+    const rootRef = firebase.database().ref();
+    const gifRef = rootRef.child('last_gif');
+    gifRef.on('value', snap => {
+      this.setState({
+        url: snap.val().url,
+        mimetype: snap.val().mimetype
+      });
+    })
+  }
+
   render() {
+    let GIFDisplay;
+    if(this.state.mimetype === "image/gif") {
+      GIFDisplay = <img src={this.state.url} alt="An amazing GIF"/>;
+    } else if (this.state.mimetype === "video/mp4") {
+      GIFDisplay = <ReactPlayer url={this.state.url} playing loop />;
+    } else {
+      GIFDisplay = <p> Not supposed to render anything for the following mimetype : {this.state.mimetype} </p>;
+    }
+
+
+
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>GIF WALL</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {GIFDisplay}
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import * as firebase from 'firebase';
+
+var config = {
+    apiKey: "AIzaSyBLrKMTIoiiFpDW92x0uIpQutOQ_eoTFrM",
+    authDomain: "gif-wall.firebaseapp.com",
+    databaseURL: "https://gif-wall.firebaseio.com",
+    storageBucket: "gif-wall.appspot.com",
+    messagingSenderId: "264236050372"
+  };
+firebase.initializeApp(config);
 
 ReactDOM.render(
   <App />,
